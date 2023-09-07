92d75506660d9c7b0663dc55ba90a36d15a564f0
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-09-07 22:48:01 +0330

    implemented adding to localStorage and random color generate

--- components/Note/Note.tsx
+++ components/Note/Note.tsx
@@ -1,16 +1,27 @@
 
+import { useSpring, animated } from '@react-spring/web'
+import { useDrag } from '@use-gesture/react'
 const Note = (props: {
     content: string;
     bgColor: string;
     rotation: number;
 }) => {
+    const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
+
+    const bind = useDrag(({ down, movement: [mx, my] }) => {
+        api.start({ x: mx, y: my, immediate: true });
+    });
+
+
     return (
-        <textarea placeholder="write a note..." defaultValue={props.content} cols={35} rows={15}
-            style={{
-                background: `${props.bgColor}`, outline: 'none', border: 'none', padding: '10px 20px', cursor: 'grab',
-                transform: `rotate(${props.rotation}deg)`
-            }}
-        />
+        <animated.div {...bind()} style={{ x, y }}>
+            <textarea placeholder="write a note..." defaultValue={props.content} cols={35} rows={15}
+                style={{
+                    background: `${props.bgColor}`, outline: 'none', border: 'none', padding: '10px 20px', cursor: 'grab',
+                    transform: `rotate(${props.rotation}deg)` , position : 'absolute'
+                }}
+            />
+        </animated.div>
     );
 };
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,71 @@
-import './App.css'
-import Note from '../components/Note/Note'
-import { useSpring, animated } from '@react-spring/web'
-import { useDrag } from '@use-gesture/react'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Note from '../components/Note/Note';
+import { useSpring, animated } from '@react-spring/web';
+import { useDrag } from '@use-gesture/react';
+
+interface NoteData {
+  content: string;
+  bgColor: string;
+  rotation: number;
+}
 
 function App() {
+  const storedNotes: NoteData[] = JSON.parse(localStorage.getItem('notes') || '[]');
 
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
+  const bgColors = ['#d070af', '#2fb4ab', '#d9a75f'];
 
-  const bind = useDrag(({ down, movement: [mx, my] }) => {
-    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down })
-  })
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * bgColors.length);
+    return bgColors[randomIndex];
+  };
+
+  const [notes, setNotes] = useState<NoteData[]>(storedNotes);
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
+
+  const addNote = () => {
+    const newNote: NoteData = {
+      content: 'New Note',
+      bgColor: getRandomColor(),
+      rotation: 4,
+    };
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
+
+  const handleOutsideClick = (e: MouseEvent) => {
+    const notesContainer = document.querySelector('.notes-container');
+    if (notesContainer && !notesContainer.contains(e.target as Node)) {
+      addNote();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
   return (
     <div>
-      <animated.div {...bind()} style={{ x, y }}>
-        <Note content='content of note 1' bgColor='#2fb4ab' rotation={4} />
-      </animated.div>
+      <div className="notes-container">
+        {notes.map((item, index) => {
+          return (
+            <Note
+              key={index}
+              content={item.content}
+              bgColor={item.bgColor}
+              rotation={item.rotation}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
