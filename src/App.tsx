085a81c0fd40dad9759c8480076a37044f2bb221
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-09-07 22:00:36 +0330

    implemented dragging notes with use-gesture

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
 import Note from '../components/Note/Note'
+import { useSpring, animated } from '@react-spring/web'
+import { useDrag } from '@use-gesture/react'
 
 function App() {
 
+  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
+
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down })
+  })
+
   return (
     <div>
-      <Note content='content of note 1' bgColor='#2fb4ab' rotation={4} />
-      
+      <animated.div {...bind()} style={{ x, y }}>
+        <Note content='content of note 1' bgColor='#2fb4ab' rotation={4} />
+      </animated.div>
     </div>
   )
 }
